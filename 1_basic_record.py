e7edba0db4f85e4d0ec50876cb3cc23bc7892772
Shubham Punekar <srp3003@gmail.com>
2018-05-06 00:36:33 +0530

    adds script to record audio as numpy arrays, and store it in wav

---
+++ 1_basic_record.py
@@ -0,0 +1,80 @@
+import os
+import pyaudio
+import numpy as np
+import pprint
+import wave
+
+'''
+Remove the warnings from alsa when creating pyaudio.PyAudio object
+-------------------------------------------------------------------
+'''
+from ctypes import *
+from contextlib import contextmanager
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+@contextmanager
+def noalsaerr():
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+'''
+-------------------------------------------------------------------
+'''
+
+
+# TODO : make this a cmd arg
+DEVICE_HW = "hw:2,0"
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = 1024
+RECORD_SECONDS = 5
+OUTPUT_FILE = os.path.dirname(os.path.realpath(__file__)) + "wavs/pyaudio_record.wav"
+
+with noalsaerr():
+    p = pyaudio.PyAudio() # start the PyAudio class
+
+# Get number of devices, iterate over each device for info, 
+# and select the device matching hw signature
+pp = pprint.PrettyPrinter(indent=4)
+for x in range(0,p.get_device_count()):
+    info = p.get_device_info_by_index(x)    
+    if "hw:2,0" in info["name"]:
+        pp.pprint(p.get_device_info_by_index(x))
+        chosen_device_index = info["index"]
+        chosen_device_name = info["name"]
+
+print("CHOSEN DEVICE : " + "index -> " + str(chosen_device_index) + ", name -> " + str(chosen_device_name))
+
+# open stream with this device
+stream = p.open(format=FORMAT, channels=CHANNELS,rate=RATE,input_device_index=chosen_device_index, input=True,
+                frames_per_buffer=CHUNK) # uses default input device
+
+# write to a wav file
+
+wavFile = wave.open("wavs/pyaudio_record.wav", "wb")
+wavFile.setnchannels(1)
+wavFile.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+wavFile.setframerate(16000)
+
+# create a numpy array holding a single read of audio data
+
+# we get numpy array for scipy format
+# and we save a wav file using wave module
+for i in range(0, int(RATE/CHUNK * RECORD_SECONDS)):
+    streamData = stream.read(CHUNK)
+    wavFile.writeframes(streamData)
+    data = np.fromstring(streamData,dtype=np.int16)
+    print(str(data) + " -> " + str(len(data)) + " samples")
+
+# close the stream 
+wavFile.close()
+stream.stop_stream()
+stream.close()
+p.terminate()
---
+++ README.md
@@ -0,0 +1,26 @@
+# Realtime Audio with pyaudio#
+
+- install alsa-utils, it bundles alsamixer, arecord and aplay
+- add user to "audio" group (may be required, if alsamixer is to be used as remote user)
+
+- To display list of devices that can be used for recording
+    arecord --list-devices
+    Here, note the "card number" and "device number" -> usually, "hw:2,0"
+    NOTE : To open a stream, select the device that has "hw:2,0" in its 'name' parameter, when queried with pyaudio object's get_device_info_by_index() method
+
+- To set mic levels, use *alsamixer.* In alsamixer : 
+    - F6 - select sound card
+    - F4 - capture device
+    - Set the mic input here, if the input is too noisy
+    - ESC to exit, settings are saved
+
+- To record,
+    arecord -f S16_LE -c 1 -r 16000 -D hw:<card number>,<device number> -d 5 test.wav
+    - f : specifies format, S16_LE is 16-bit little endian
+    - c : channels, set to 1
+    - -r : specifies sampling rate
+    - D hw:2,0 : specifies the device, for USB camera, it usually is card 2, device 0
+    - -d : delay, audio file length
+
+- To play
+    aplay test.wav
